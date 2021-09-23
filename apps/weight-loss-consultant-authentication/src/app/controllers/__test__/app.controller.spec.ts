156a2d362befa07976470fab8e705f5521e2a1ba
tientruong2000 <tientt1938@gmail.com>
2021-09-23 23:25:29 +0700

    FUSE-16 fix to pass test

--- apps/weight-loss-consultant-authentication/src/app/controllers/__test__/app.controller.spec.ts
+++ apps/weight-loss-consultant-authentication/src/app/controllers/__test__/app.controller.spec.ts
@@ -2,16 +2,26 @@ import { AppController } from '../app.controller';
 import { CustomerService } from '../../services/customer.service';
 import { AuthenticationService } from '../../services/authentication.service';
 import { TrainerDTO } from '../../dtos/trainer.dto';
-import { CustomerDTO } from '../../dtos/customer.dto';
+import { TrainerService } from '../../services/trainer.service';
+import { MailService } from '../../services/mail.service';
+import { ResetPasswordTokenService } from '../../services/reset-password-token.service';
+import { AccountService } from '../../services/account.service';
 
 describe('The AuthenticationController', () => {
 
   let appController: AppController;
   let customerService: CustomerService;
+  let trainerService: TrainerService;
+  let mailService: MailService;
+  let resetPasswordTokenService: ResetPasswordTokenService;
+  let accountService :AccountService;
   let authenticationService: AuthenticationService;
 
   beforeEach(() => {
-    appController = new AppController(customerService, authenticationService);
+    appController = new AppController(customerService,
+      trainerService,
+      authenticationService,
+      mailService, resetPasswordTokenService, accountService);
 	});
 
   describe('Test user login', () => {
