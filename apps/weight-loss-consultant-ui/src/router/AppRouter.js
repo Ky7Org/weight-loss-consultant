bad5b24cd74eb2235cb8bcd48b618590d268e85f
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2021-09-30 13:07:25 +0700

    [FUSE-41] - Integration Sign-In/Sign-Up (#27)
    - Integration sign in
    - Setup Redux
    - Setup Axios
    - Integration List User
    - Sort List

---
+++ apps/weight-loss-consultant-ui/src/router/AppRouter.js
@@ -0,0 +1,21 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
+import PrivateRoute from '../router/PrivateRoute';
+import PublicRoute from '../router/PublicRoute';
+import Signin from '../pages/auth/Signin/Signin';
+import ErrorPage from '../pages/ErrorPages/ErrorPages';
+import ListUser from '../components/ListUser/ListUser';
+export default function AppRouter() {
+  return (
+    <Switch>
+      <PublicRoute path="/auth/signin" component={Signin} exact={true} />
+      <PrivateRoute path="/home" component={HomePage} exact={true} />
+      <PrivateRoute path="/admin/user/list" component={ListUser} exact={true} />
+      <Redirect exact from="/" to="/home" />
+      <Route exact path="/page-not-found">
+        <ErrorPage code={404} />
+      </Route>
+      <Redirect from="*" to="/page-not-found" />
+    </Switch>
+  );
+}
