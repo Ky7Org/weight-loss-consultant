c716977aac85b65da02b409b08f2e8f5e84163a7
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2021-09-30 12:59:37 +0700

    Integration Sign-In/Sign-Up
    - Integration sign in
    - Setup Redux
    - Setup Axios
    - Integration List User
    - Sort List

---
+++ apps/weight-loss-consultant-ui/src/components/header/header.jsx
@@ -0,0 +1,23 @@
+import { Row, Col } from 'antd';
+import { ImageCustom } from '../../components/image';
+
+const HeaderLayout = () => {
+  return (
+    <>
+      <Row style={{ padding: '10px' }}>
+        <Col xs={8} xl={10} />
+        <Col xs={8} xl={4}>
+          <ImageCustom path="/images/wcs.svg" />
+        </Col>
+        <Col xs={8} xl={10} />
+      </Row>
+      <Row>
+        <Col xs={24} xl={24}>
+          <hr />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default HeaderLayout;
