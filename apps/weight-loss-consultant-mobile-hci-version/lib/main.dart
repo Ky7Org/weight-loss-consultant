922d15782eee1ba7ea5bd820a2adda53279c8710
NghiemTD <trnduynghim2909@gmail.com>
2021-10-02 23:18:20 +0700

    mplement ui for repost screen

--- apps/weight-loss-consultant-mobile-hci-version/lib/main.dart
+++ apps/weight-loss-consultant-mobile-hci-version/lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:weight_loss_consultant_mobile_hci_version/screens/login_screen.dart';
 import 'package:weight_loss_consultant_mobile_hci_version/screens/recover_password.dart';
 import 'package:weight_loss_consultant_mobile_hci_version/screens/sign_up_screen.dart';
+import 'package:weight_loss_consultant_mobile_hci_version/screens/report_screen.dart';
 
 void main(){
   runApp(MaterialApp(
     initialRoute: "/",
     routes: {
-      "/": (context) => RecoverScreen(),
+      "/": (context) => const ReportScreen( ),
     },
   ));
 }
