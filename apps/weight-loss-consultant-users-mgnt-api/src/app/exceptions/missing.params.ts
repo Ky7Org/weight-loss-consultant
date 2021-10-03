6b21864623dd274c02b54073fec1416e02ca2e6c
Son Pham <phamcaoson1999@gmail.com>
2021-10-04 01:59:11 +0700

    [FUSE-55] - Implement sorting and pagination for Admin, Customer And Trainer service

---
+++ apps/weight-loss-consultant-users-mgnt-api/src/app/exceptions/missing.params.ts
@@ -0,0 +1,7 @@
+import {HttpException, HttpStatus} from "@nestjs/common";
+
+export class MissingParamsException extends HttpException {
+  constructor() {
+    super('Unable to process the contained instructions. Maybe you are missing some parameters in the URL !', HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+}
