f2602cdc18c97167be1a01700ef79d018d53fe21
Ngô Nguyên Bằng <webcam1362000@gmail.com>
2021-10-11 01:24:46 +0700

    [FUSE-71] - Major dependencies upgrade (#61)

--- apps/weight-loss-consultant-ui/src/polyfills.ts
+++ apps/weight-loss-consultant-ui/src/polyfills.ts
@@ -1,5 +1,5 @@
 /**
- * Polyfill stable language features. These imports will be optimized by `@babel/preset-env`.
+ * Polyfill stable language features. These imports will be optimized by `@babel/preset-.env`.
  *
  * See: https://github.com/zloirock/core-js#babel
  */
