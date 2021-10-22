ae568abea633f8ccb41a6882a83a8f180bbd7b66
duynghim <74170051+duynghim@users.noreply.github.com>
2021-10-22 17:18:35 +0700

    [FUSE-107] - Implement UI for Trainer Create Package Screen and Customer Create Campaign Screen

---
+++ apps/weight-loss-consultant-mobile/lib/pages/trainer/no_packages_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:weight_loss_consultant_mobile/constants/app_colors.dart';
+
+class NoPackagesPage extends StatefulWidget {
+  const NoPackagesPage({Key? key}) : super(key: key);
+
+  @override
+  _NoPackagesPageState createState() => _NoPackagesPageState();
+}
+
+class _NoPackagesPageState extends State<NoPackagesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child:
+            SvgPicture.asset("assets/fake-image/no-package.svg"),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Text(
+              'No Packages',
+              style: TextStyle(
+                  color: AppColors.PRIMARY_WORD_COLOR,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w700
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Text(
+              'You dont have any package.',
+               style: TextStyle(
+                 color: AppColors.PRIMARY_WORD_COLOR,
+                 fontSize: 15,
+                 fontWeight: FontWeight.w400
+               ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Create one?',
+              style: TextStyle(
+                  color: AppColors.PRIMARY_WORD_COLOR,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Center(
+            child: GestureDetector(
+              child: SvgPicture.asset("assets/fake-image/add-button.svg"),
+              onTap: (){
+                print('Ahihi');
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
