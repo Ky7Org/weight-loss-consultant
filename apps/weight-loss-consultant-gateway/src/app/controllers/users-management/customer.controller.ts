386d456be133eb995d8875fb53cf59bf49fe5dab
Son Pham <phamcaoson1999@gmail.com>
2021-10-26 23:51:41 +0700

    [FUSE-HOTFIX] - I had to used raw sql to satisfy Tien-san

--- apps/weight-loss-consultant-gateway/src/app/controllers/users-management/customer.controller.ts
+++ apps/weight-loss-consultant-gateway/src/app/controllers/users-management/customer.controller.ts
@@ -29,7 +29,7 @@ export class CustomerController {
   @Get('/tienAPI/:email')
   async getDetail(@Param('email') email: string, @Res() res) {
     try {
-      const customer = await this.customerService.viewDetail(email);
+      const customer = await this.customerService.viewDetailSpecial(email);
       if (customer === undefined) {
         const error = {
           statusCode : 404,
