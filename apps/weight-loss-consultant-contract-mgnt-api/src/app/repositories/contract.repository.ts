dd21715d31f6f72e6e9f0377d6f7171f23979674
Son Pham <phamcaoson1999@gmail.com>
2021-10-27 23:37:12 +0700

    [FUSE-UPDATE] - Give trainerEmail, return list packages of that trainer API

--- apps/weight-loss-consultant-contract-mgnt-api/src/app/repositories/contract.repository.ts
+++ apps/weight-loss-consultant-contract-mgnt-api/src/app/repositories/contract.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import {EntityRepository, Repository} from 'typeorm';
 import {ContractEntity} from "../entities/contract.entity";
 
 @EntityRepository(ContractEntity)
