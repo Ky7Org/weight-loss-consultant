24b5b31326cf25e267354a992e5286d82e9b0762
duynghim <74170051+duynghim@users.noreply.github.com>
2021-11-04 23:10:01 +0700

    [FUSE-124] - Fix Some Things

--- apps/weight-loss-consultant-mobile/lib/pages/customer/upcoming_training_page.dart
+++ apps/weight-loss-consultant-mobile/lib/pages/customer/upcoming_training_page.dart
@@ -1,58 +1,117 @@
+import 'dart:convert';
+
+import 'package:collection/src/iterable_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weight_loss_consultant_mobile/constants/app_colors.dart';
+import 'package:weight_loss_consultant_mobile/models/account_model.dart';
+import 'package:weight_loss_consultant_mobile/models/campaign_model.dart';
+import 'package:weight_loss_consultant_mobile/models/package_model.dart';
 import 'package:weight_loss_consultant_mobile/pages/components/generic_app_bar.dart';
 import 'package:weight_loss_consultant_mobile/routings/route_paths.dart';
+import 'package:weight_loss_consultant_mobile/services/customer_service.dart';
 
 class UpcomingTrainingPage extends StatefulWidget {
-  const UpcomingTrainingPage({Key? key}) : super(key: key);
+  PackageModel? packageModel;
+
+  UpcomingTrainingPage({Key? key, this.packageModel}) : super(key: key);
 
   @override
   _UpcomingTrainingPageState createState() => _UpcomingTrainingPageState();
 }
 
 class _UpcomingTrainingPageState extends State<UpcomingTrainingPage> {
+  AccountModel user = AccountModel(email: "", fullname: "");
+  PackageModel? package;
+  CampaignModel? ongoingCampaign;
 
-  Widget _heading(String heading){
+  Future<void> initAccount() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userJSON = prefs.getString('ACCOUNT');
+    if (userJSON is String) {
+      Map<String, dynamic> userMap = jsonDecode(userJSON);
+      user = AccountModel.fromJson(userMap);
+    }
+  }
+
+  Future<void> saveAccount() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("ACCOUNT", jsonEncode(user.toJson()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      initAccount().then((value) {
+        setState(() {});
+      });
+    });
+  }
+
+  Widget _heading(String heading) {
     return Text(
       heading,
       style: TextStyle(
-        color: AppColors.PRIMARY_WORD_COLOR,
-        fontSize: 18,
-        fontWeight: FontWeight.w700
-      ),
+          color: AppColors.PRIMARY_WORD_COLOR,
+          fontSize: 18,
+          fontWeight: FontWeight.w700),
     );
   }
 
-
-  Widget _content(String content, String title){
+  Widget _content(String content, String title) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(title, style: TextStyle(
-              color: AppColors.PRIMARY_WORD_COLOR,
-              fontWeight: FontWeight.w400,
-              fontSize: 13
-          ),),
+          Text(
+            content,
+            style: TextStyle(
+                color: AppColors.PRIMARY_WORD_COLOR,
+                fontWeight: FontWeight.w900,
+                fontSize: 15),
+          ),
           const SizedBox(
             height: 5,
           ),
-          Text(content, style: TextStyle(
-              color: AppColors.PRIMARY_WORD_COLOR,
-              fontWeight: FontWeight.w900,
-              fontSize: 15
-          ),),
+          Text(
+            title,
+            style: TextStyle(
+                color: AppColors.PRIMARY_WORD_COLOR,
+                fontWeight: FontWeight.w400,
+                fontSize: 13),
+          ),
         ],
       ),
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
+    Widget avatarOfUser() {
+      if (widget.packageModel!.trainer!.profileImage!.isNotEmpty) {
+        return Image(
+            image: NetworkImage(widget.packageModel!.trainer!.profileImage!),
+            width: 100,
+            height: 100,
+            fit: BoxFit.fill);
+      }
+      return const Image(
+          image: AssetImage("assets/fake-image/fake-trainer-avatar.jpg"),
+          width: 100,
+          height: 100,
+
+          fit: BoxFit.fill);
+    }
+
+    var date = DateFormat("MMMM-dd-yyyy")
+        .format(DateTime.fromMillisecondsSinceEpoch(
+            int.parse(widget.packageModel!.trainer!.dob ?? "")))
+        .toString();
     return Scaffold(
-      appBar: GenericAppBar.builder("Upcoming Training"),
+      appBar: GenericAppBar.builder("Call Your Trainer"),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
         child: Container(
@@ -74,15 +133,7 @@ class _UpcomingTrainingPageState extends State<UpcomingTrainingPage> {
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    flex: 2,
-                    child: SizedBox(
-                      height: 80,
-                      width: 80,
-                      child: Image.asset(
-                          'assets/fake-image/fake-trainer-avatar.jpg'),
-                    ),
-                  ),
+                  avatarOfUser(),
                   const SizedBox(
                     width: 15,
                   ),
@@ -96,9 +147,9 @@ class _UpcomingTrainingPageState extends State<UpcomingTrainingPage> {
                           children: [
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
+                              children: [
                                 Text(
-                                  'Mr.Bang Ngo',
+                                  widget.packageModel!.trainer!.fullname ?? "",
                                   style: TextStyle(
                                     color: Color(0xFF0D3F67),
                                     fontWeight: FontWeight.w700,
@@ -112,17 +163,17 @@ class _UpcomingTrainingPageState extends State<UpcomingTrainingPage> {
                               ],
                             ),
                             Text(
-                              'Boxing | Gym Trainer',
+                              widget.packageModel!.trainer!.phone ?? "",
                               style: TextStyle(
                                   color: Color(0xFFB6C5D1),
                                   fontSize: 13,
                                   fontWeight: FontWeight.w900),
                             ),
                             Text(
-                              '30 November 2021 17:00',
+                              date,
                               style: TextStyle(
                                   color: Color(0xFF0D3F67),
-                                  fontSize: 11,
+                                  fontSize: 15,
                                   fontWeight: FontWeight.w400),
                             )
                           ],
@@ -138,61 +189,74 @@ class _UpcomingTrainingPageState extends State<UpcomingTrainingPage> {
                 children: [
                   Container(
                     child: Row(
-                      children: [Container(
-                          width: 42,
-                          height: 42,
-                          margin: EdgeInsets.only(right: 10),
-                          child: Icon(
-                            Icons.play_arrow_outlined,
-                            color: AppColors.PRIMARY_COLOR,
-                            size: 28,
-                          ),
-                          decoration: BoxDecoration(
-                              color: Color(0xFFFDD9D9),
-                              borderRadius: BorderRadius.circular(10))), Text(
-                        'Video Call',
-                        style: TextStyle(
-                            color: AppColors.PRIMARY_WORD_COLOR,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w400),
-                      ),],
+                      children: [
+                        Container(
+                            width: 42,
+                            height: 42,
+                            margin: EdgeInsets.only(right: 10),
+                            child: Icon(
+                              Icons.play_arrow_outlined,
+                              color: AppColors.PRIMARY_COLOR,
+                              size: 28,
+                            ),
+                            decoration: BoxDecoration(
+                                color: Color(0xFFFDD9D9),
+                                borderRadius: BorderRadius.circular(10))),
+                        Text(
+                          'Video Call',
+                          style: TextStyle(
+                              color: AppColors.PRIMARY_WORD_COLOR,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
                     ),
                   ),
                   FlatButton(
-                      color: AppColors.PRIMARY_COLOR,
-                      onPressed: () {
-                        Navigator.pushNamed(context, RoutePath.videoCallPage);
-                      },
-                      child: Row(
-                        children: [
-                          Text(
-                            'Start Now',
-                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 13),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Icon(Icons.arrow_forward, color: Colors.white, size: 18,)
-                        ],
-                      ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18)
+                    color: AppColors.PRIMARY_COLOR,
+                    onPressed: () {
+                      Navigator.pushNamed(context, RoutePath.videoCallPage);
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          'Start Now',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 13),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                          size: 18,
+                        )
+                      ],
                     ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)),
                   )
                 ],
               ),
               Divider(
                 thickness: 1,
               ),
-              _heading("Cardio Training"),
-              _content("Weight loss consultant", "Training Program"),
-              _content("Burn fat", "Description"),
-              Divider(
+              _heading("Applied Package"),
+              const SizedBox(height: 10),
+              _content("Package name", widget.packageModel!.name ?? ""),
+              _content(
+                  "Exercise Plan", widget.packageModel!.exercisePlan ?? ""),
+              _content("Diet Plan", widget.packageModel!.dietPlan ?? ""),
+              const Divider(
                 thickness: 1,
               ),
               _heading("Note"),
-              _content("600 Calories", "Target"),
-              _content("Eat before train...", "Trainer guide"),
+              const SizedBox(height: 10),
+              _content(
+                  "Schedule Description", widget.packageModel!.schedule ?? ""),
             ],
           ),
         ),
--- apps/weight-loss-consultant-mobile/lib/routings/route_generator.dart
+++ apps/weight-loss-consultant-mobile/lib/routings/route_generator.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weight_loss_consultant_mobile/models/campaign_model.dart';
+import 'package:weight_loss_consultant_mobile/models/customer_campaign_model.dart';
+import 'package:weight_loss_consultant_mobile/models/package_model.dart';
+import 'package:weight_loss_consultant_mobile/models/trainer_model.dart';
 import 'package:weight_loss_consultant_mobile/pages/customer/customer_applied_package_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/customer/customer_campaign_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/customer/customer_create_campaign.dart';
@@ -29,6 +33,7 @@ import 'package:weight_loss_consultant_mobile/pages/authentication/recorver_pass
 import 'package:weight_loss_consultant_mobile/pages/authentication/reset_password_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/register/trainer_register_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/register/register_sucessful_page.dart';
+import 'package:weight_loss_consultant_mobile/pages/trainer/upcoming_training_trainer.dart';
 import 'package:weight_loss_consultant_mobile/pages/trainer/create_packages_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/trainer/no_packages_page.dart';
 import 'package:weight_loss_consultant_mobile/pages/trainer/trainer_feedback_report_page.dart';
@@ -43,231 +48,273 @@ import 'package:weight_loss_consultant_mobile/pages/trainer/trainer_available_ca
 import 'package:weight_loss_consultant_mobile/pages/trainer/trainer_view_list_package_page.dart';
 import 'package:weight_loss_consultant_mobile/routings/route_paths.dart';
 
-
-class RouteGenerator{
-  static Route<dynamic> generateRoute(RouteSettings settings){
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
-    switch (settings.name){
+    switch (settings.name) {
       case RoutePath.initialPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const InitialPage();
         });
       case RoutePath.getStartedPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const GetStartedPage();
         });
       case RoutePath.trainerRegisterPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const TrainerRegisterPage();
         });
-      case RoutePath.trainerRegisterSuccessfullyPage :
-        if (args is Map<dynamic, dynamic>){
-          return MaterialPageRoute(builder: (_){
-            return RegisterSuccessfulPage(data: args,);
+      case RoutePath.trainerRegisterSuccessfullyPage:
+        if (args is Map<dynamic, dynamic>) {
+          return MaterialPageRoute(builder: (_) {
+            return RegisterSuccessfulPage(
+              data: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.loginPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const LoginPage();
         });
       case RoutePath.recoverPasswordPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const RecoverPasswordPage();
         });
       case RoutePath.customerHomePage:
-          return MaterialPageRoute(builder: (_){
-            return const CustomerHomePage();
+        return MaterialPageRoute(builder: (_) {
+          return const CustomerHomePage();
         });
       case RoutePath.chatPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const ChatPage();
         });
       case RoutePath.customerDetailPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CustomerDetailPage();
         });
       case RoutePath.resetPasswordPage:
-        if (args is Map<dynamic, dynamic>){
-          return MaterialPageRoute(builder: (_){
-            return ResetPasswordPage(data: args,);
+        if (args is Map<dynamic, dynamic>) {
+          return MaterialPageRoute(builder: (_) {
+            return ResetPasswordPage(
+              data: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.registerPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const RegisterPage();
         });
       case RoutePath.myMessagePage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const MyMessagesPage();
         });
       case RoutePath.settingPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const SettingScreen();
         });
       case RoutePath.customerTodoPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CustomerTodoPage();
         });
       case RoutePath.trainerHomePage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const TrainerHomePage();
         });
       case RoutePath.upcomingTrainingPage:
-        return MaterialPageRoute(builder: (_){
-          return const UpcomingTrainingPage();
-        });
+        if (args is PackageModel) {
+          return MaterialPageRoute(builder: (_) {
+            return UpcomingTrainingPage(
+              packageModel: args,
+            );
+          });
+        }
+        return _errorRoute();
+      case RoutePath.trainerUpcomingPage:
+        if (args is CustomerCampaignModel) {
+          return MaterialPageRoute(builder: (_) {
+            return UpcomingTrainingTrainerPage(
+              campaignModel: args,
+            );
+          });
+        }
+        return _errorRoute();
       case RoutePath.videoCallPage:
-        return MaterialPageRoute(builder: (_){
-          return const VideoCallPage();
+        return MaterialPageRoute(builder: (_) {
+          return VideoCallPage();
         });
       case RoutePath.endCallPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const EndingCallPage();
         });
       case RoutePath.createCampaignPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CreateCampaignPage();
         });
       case RoutePath.noPackagePage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const NoPackagesPage();
         });
       case RoutePath.createPackagesPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CreatePackagesPage();
         });
       case RoutePath.editProfilePage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const EditProfilePage();
         });
       case RoutePath.customerCampaignPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CustomerCampaignPage();
         });
       case RoutePath.trainerPackagePage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const TrainerPackagePage();
         });
       case RoutePath.trainerViewCampaignDetailPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
             return TrainerViewCampaignDetailPage(campaignID: args);
           });
         }
         return _errorRoute();
       case RoutePath.trainerViewListCampaignPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const TrainerAvailableCampaignPage();
         });
       case RoutePath.trainerViewListPackagePage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerViewListPackagePage(campaignId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerViewListPackagePage(
+              campaignId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerUpdateCampaignPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerUpdateCampaignPage(campaignID: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerUpdateCampaignPage(
+              campaignID: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerAppliedPackagePage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerAppliedPackagePage(campaignId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerAppliedPackagePage(
+              campaignId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerPackageDetailPage:
-        if (args is Map<String, dynamic>){
-          return MaterialPageRoute(builder: (_){
-            return CustomerPackageDetail(data: args,);
+        if (args is Map<String, dynamic>) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerPackageDetail(
+              data: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.trainerUpdatePackagePage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerUpdatePackagePage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerUpdatePackagePage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerOverallReportPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CustomerOverallReportPage();
         });
       case RoutePath.customerOverallReportCalendarViewPage:
-        return MaterialPageRoute(builder: (_){
+        return MaterialPageRoute(builder: (_) {
           return const CustomerOverallReportCalendarViewPage();
         });
       case RoutePath.customerOngoingCampaignPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerOnGoingCampaignPage(campaignId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerOnGoingCampaignPage(
+              campaignId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerMakeReportPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerMakeReportPage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerMakeReportPage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.trainerFeedbackReportPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerFeedbackReportPage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerFeedbackReportPage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerReportHistoryPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerReportHistoryPage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerReportHistoryPage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.customerReportDetailPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return CustomerReportDetailPage(reportId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return CustomerReportDetailPage(
+              reportId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.trainerOnGoingPackageDetailPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerOnGoingPackageDetailPage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerOnGoingPackageDetailPage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.trainerReportHistoryPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerReportHistoryPage(packageId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerReportHistoryPage(
+              packageId: args,
+            );
           });
         }
         return _errorRoute();
       case RoutePath.trainerReportDetailPage:
-        if (args is int){
-          return MaterialPageRoute(builder: (_){
-            return TrainerReportDetailPage(reportId: args,);
+        if (args is int) {
+          return MaterialPageRoute(builder: (_) {
+            return TrainerReportDetailPage(
+              reportId: args,
+            );
           });
         }
         return _errorRoute();
       default:
         return _errorRoute();
-
     }
   }
 
-  static Route<dynamic> _errorRoute(){
-    return MaterialPageRoute(builder: (_){
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
       return Scaffold(
         appBar: GenericAppBar.builder("Something went wrong"),
         body: const Center(
